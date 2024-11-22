456217d4235f5bc53297e7cd53b1e7486fd80dbb
Frank Martinez <mnesarco@gmail.com>
2024-11-22 10:59:26 -0500

    add preference for import as sketch

--- freecad/mnesarco/svg/svg_file.py
+++ freecad/mnesarco/svg/svg_file.py
@@ -1,46 +1,52 @@
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright (C) 2021 Frank David Martinez M. <https://github.com/mnesarco/>
-# 
+#
 # This file is part of Mnesarco Utils.
-# 
+#
 # Mnesarco Utils is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Utils is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Mnesarco Utils.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
-from typing import List, Tuple, Iterable
+import re
+from contextlib import suppress
+from itertools import count
+from pathlib import Path
+from typing import Iterable, List, Tuple
+
 import FreeCAD as App  # type: ignore
-import FreeCADGui as Gui # type: ignore
+import FreeCADGui as Gui  # type: ignore
 import importSVG as svg  # type: ignore
 import Part  # type: ignore
-import re
-from pathlib import Path
-from itertools import count
-
-import PySide.QtCore as QtCore # type: ignore
-import PySide.QtGui as QtGui # type: ignore
+import PySide.QtCore as QtCore  # type: ignore
+import PySide.QtGui as QtGui  # type: ignore
 
 from freecad.mnesarco.utils.extension import def_log
 from freecad.mnesarco.vendor.fpo import (
-    proxy, 
-    view_proxy, 
-    PropertyFile, 
-    PropertyFileIncluded, 
-    PropertyStringList, 
     PropertyBool,
-    PropertyMode)
+    PropertyFile,
+    PropertyFileIncluded,
+    PropertyMode,
+    PropertyStringList,
+    proxy,
+    view_proxy,
+    Preference
+)
 
 log_err = def_log('SvgFile')
+
+# Preferences
+default_import_as_sketch = Preference(group="MnesarcoUtils/Svg", name="Import as Sketch", default=False)
 
 SELECTOR_PATTERN = re.compile(r'((?P<name>\w+)\s*:\s*)?\s*(?P<pat>.*)')
 WORD_PATTERN = re.compile(r'\w+')
@@ -71,7 +77,7 @@ def upsert(shape: Part.Shape, name: str, doc: App.Document, parent: App.Document
     """
     obj = doc.getObject(name)
     if as_sketch:
-        import Draft # type: ignore
+        import Draft  # type: ignore
         if obj:
             if hasattr(obj, 'delGeometries'):
                 obj.delGeometries([i for i in range(obj.GeometryCount)])
@@ -89,12 +95,12 @@ def upsert(shape: Part.Shape, name: str, doc: App.Document, parent: App.Document
         if not obj:
             obj = doc.addObject('Part::FeatureExt', name)
             obj.Label = name
-            obj.addExtension('Part::AttachExtensionPython')        
+            obj.addExtension('Part::AttachExtensionPython')
         obj.Shape = shape
         if not obj.getParent():
             parent.addObject(obj)
     return obj
-    
+
 
 def parse_selector(name: str|None, pattern: str|None, id_prefix: str, id_gen: count) -> Tuple[str, re.Pattern, str] | None:
     """
@@ -109,22 +115,22 @@ def parse_selector(name: str|None, pattern: str|None, id_prefix: str, id_gen: co
     if not isinstance(name, str):
         name = ''
     if not isinstance(pattern, str):
-        pattern = ''        
-    
+        pattern = ''
+
     name = name.strip()
     pattern = pattern.strip()
 
     # No pattern -> no match
     if not pattern:
         return None
-    
+
     src_pattern = pattern
 
     # in case of only a word, it is used as name and pattern
     # example> path1
     if not name and WORD_PATTERN.fullmatch(pattern):
         name = pattern
-    
+
     # name MUST be a word or generated id
     if not WORD_PATTERN.fullmatch(name):
         name = f'{id_prefix}_{next(id_gen)}'
@@ -181,10 +187,13 @@ class SvgFileView:
 
 @proxy(object_type='App::DocumentObjectGroupPython', view_proxy=SvgFileView)
 class SvgFile:
-    source_file = PropertyFile(description = 'Path to the external svg file')
-    file = PropertyFileIncluded(description = 'Path to the internal svg file', mode=PropertyMode.Hidden)
+    source_file = PropertyFile(description='Path to the external svg file')
+    file = PropertyFileIncluded(description='Path to the internal svg file', mode=PropertyMode.Hidden)
     select = PropertyStringList(description="Id Patterns", default=['all:.*'])
     as_sketches = PropertyBool(description="Import geometry as sketches", default=False)
+
+    def on_create(self, obj):
+        self.as_sketches = default_import_as_sketch()
 
     @source_file.observer
     def on_source_change(self, obj, path):
@@ -204,7 +213,7 @@ class SvgFile:
         changed = False
         doc = obj.Document
         for name, pattern, src_pattern in parse_selectors(self.select, obj.Name):
-            child = doc.getObject(name) 
+            child = doc.getObject(name)
             if not child or child.getParent() is obj:
                 result.append((name, pattern, src_pattern))
             else:
@@ -242,12 +251,12 @@ class SvgFile:
                     log_err(f"Group not found: {name}: {xml_id}")
 
 
-    def on_execute(self, obj):        
+    def on_execute(self, obj):
         selection, selection_changed = self.get_selection(obj)
         if selection_changed:
             self.select = [f"{n}:{p}" for n,pp,p in selection]
             return
-        
+
         if self.file and Path(self.file).exists() and self.select:
             doc_name = App.ActiveDocument.Name
 
@@ -271,11 +280,9 @@ class SvgFile:
                 self.extract_by_group(not_found, new_children, svg_doc)
 
             # Clean hidden import file
-            try:
+            with suppress(Exception):
                 App.closeDocument(svg_doc_name)
-            except:
-                pass
-            
+
             # Insert targets into current doc
             App.setActiveDocument(doc_name)
             updated_objects = []
